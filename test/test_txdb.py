0018e52536fe2b4e2cc9f230923c5461d99ae7ec
Mike Lin <dna@mlin.net>
2020-07-15 20:54:57 -1000

    TxDb integration test

--- test/test_txdb.py
+++ test/test_txdb.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 import subprocess
+import random
 import pytest
 import genomicsqlite
 
@@ -51,6 +52,9 @@ def genomicsqlite_txdb(txdb):
 
 def test_txdbquery(genomicsqlite_txdb):
     conn = genomicsqlite.connect(genomicsqlite_txdb, read_only=True)
+    assert next(conn.execute("PRAGMA page_size"))[0] == 16384
+
+    # one query
     results = list(
         t[0]
         for t in conn.execute(
@@ -65,4 +69,42 @@ def test_txdbquery(genomicsqlite_txdb):
         ["ENST00000416293.7", "ENST00000261733.7", "ENST00000548536.1", "ENST00000549106.1"]
     )
 
-    assert next(conn.execute("PRAGMA page_size"))[0] == 16384
+    # random queries
+    chroms = list(
+        conn.execute(
+            "SELECT tx_chrom, length FROM (SELECT tx_chrom, MAX(tx_end) AS length FROM transcript GROUP BY tx_chrom) WHERE length > 1000000"
+        )
+    )
+
+    random.seed(0xBADF00D)
+    for tbl in ("transcript", "cds"):
+        query = genomicsqlite.genomic_range_rowids_sql(conn, tbl)[1:-1]
+        pfx = "tx" if tbl == "transcript" else tbl
+        control_query = f"SELECT _rowid_ FROM {tbl} NOT INDEXED WHERE {pfx}_chrom = ? AND NOT ({pfx}_end < ? OR {pfx}_start > ?) ORDER BY _rowid_"
+
+        total_results = 0
+        for _ in range(1000):
+            chrom = random.choice(chroms)
+            beg = random.randint(0, chrom[1] - 65536)
+            end = beg + random.randint(1, random.choice([16, 256, 4096, 65536]))
+            ids = list(row[0] for row in conn.execute(query, (chrom[0], beg, end)))
+            control_ids = list(row[0] for row in conn.execute(control_query, (chrom[0], beg, end)))
+            assert ids == control_ids
+            total_results += len(control_ids)
+        assert total_results in (3802, 1341)
+
+    # join cds to exon
+    cds_exon_counts = (
+        "SELECT cds._rowid_ AS cds_id, COUNT(exon._rowid_) AS containing_exons FROM cds, exon WHERE exon._rowid_ IN "
+        + genomicsqlite.genomic_range_rowids_sql(conn, "exon", "cds_chrom", "cds_start", "cds_end")
+        + " AND cds_start >= exon_start and cds_end <= exon_end GROUP BY cds._rowid_"
+    )
+    cds_exon_count_hist = list(
+        conn.execute(
+            f"SELECT containing_exons, count(cds_id) AS cds_count FROM ({cds_exon_counts}) GROUP BY containing_exons ORDER BY containing_exons"
+        )
+    )
+    for elt in cds_exon_count_hist:
+        print(elt)
+    assert cds_exon_count_hist[0] == (1, 168266)
+    assert cds_exon_count_hist[1] == (2, 71159)
