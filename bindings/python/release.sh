004ccdc502be7591e1162944476731461e49f31b
Mike Lin <dna@mlin.net>
2020-07-21 01:35:05 -1000

    failsafe

--- bindings/python/release.sh
+++ bindings/python/release.sh
@@ -13,10 +13,13 @@ if [[ ! -f genomicsqlite/libgenomicsqlite.so ]]; then
     >&2 echo "Download the portable libgenomicsqlite.so to ${HERE}/genomicsqlite/"
     exit 1
 fi
+function cleanup {
+    # force us to put it there each time; ensures a stale version can't be left behind
+    rm -f genomicsqlite/libgenomicsqlite.so
+}
+trap cleanup EXIT
 
 rm -rf build dist *.egg-info
 python3 setup.py sdist
 echo -e "\033[0;31;5m -- Pushing $$(basename `ls -1 dist/*.tar.gz` .tar.gz) to PyPI! -- \033[0m"
 twine upload dist/*.tar.gz
-
-rm genomicsqlite/libgenomicsqlite.so
