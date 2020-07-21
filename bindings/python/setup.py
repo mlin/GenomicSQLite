b9fb05fe9c577c59baaa0d06565bf4f04d029275
Mike Lin <dna@mlin.net>
2020-07-21 01:56:20 -1000

    PyPI packaging

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 from os import path
-from setuptools import setup
+from setuptools import setup, find_packages
+from version import get_version
 
 HERE = path.abspath(path.dirname(__file__))
 with open(path.join(HERE, "README.md")) as f:
     README = f.read()
 
 setup(
+    python_requires=">=3.6",
     name="genomicsqlite",
-    version="0.0.1",
+    version=get_version(),
     url="https://github.com/mlin/GenomicSQLite",
     description="Genomics Extension for SQLite",
     long_description=README,
     long_description_content_type="text/markdown",
     author="Mike Lin",
     author_email="dna@mlin.net",
-    py_modules=["genomicsqlite"],
-    python_requires=">=3.6",
+    packages=find_packages(),
+    package_data={"": ["genomicsqlite/libgenomicsqlite.so"]},
+    include_package_data=True,
     entry_points={"console_scripts": ["genomicsqlite = genomicsqlite:_cli"]},
 )
